66a64d9582d7ff8eb855ba59621a1d406b5d2efd
Yana-Koroliuk <yana.koroliuk1@gmail.com>
2024-03-01 14:29:52 +0200

    Improve checking error of markers
    Improve check for unbalanced markers and add check for nested markers

--- src/main/java/com/koroliuk/app/Converter.java
+++ src/main/java/com/koroliuk/app/Converter.java
@@ -1,9 +1,7 @@
 package com.koroliuk.app;
 
-import java.security.Security;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Stack;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -15,11 +13,10 @@ public class Converter {
     }
 
     public String markdownToHtml() throws Exception {
-
         List<String> preBlocks = new ArrayList<>();
         String html = markdown;
 
-        Pattern prePattern = Pattern.compile("(?<![\\w`*\\u0400-\\u04FF])```(\\S(?:.*?\\S)?)```(?![\\w`*\\u0400-\\u04FF])", Pattern.DOTALL);
+        Pattern prePattern = Pattern.compile("```(.*?)```", Pattern.DOTALL);
         Matcher preMatcher = prePattern.matcher(html);
         int preIndex = 0;
         while (preMatcher.find()) {
@@ -31,6 +28,13 @@ public class Converter {
         String regexBold = "(?<![\\w`*\u0400-\u04FF])\\*\\*(\\S(?:.*?\\S)?)\\*\\*(?![\\w`*\u0400-\u04FF])";
         String regexItalic = "(?<![\\w`*\\u0400-\\u04FF])_(\\S(?:.*?\\S)?)_(?![\\w`*\\u0400-\\u04FF])";
         String regexMonospaced = "(?<![\\w`*\\u0400-\\u04FF])`(\\S(?:.*?\\S)?)`(?![\\w`*\\u0400-\\u04FF])";
+
+        List<String> boldBlocks = getMatchPatternList(regexBold, html);
+        List<String> italicBlocks = getMatchPatternList(regexItalic, html);
+        List<String> monospacedBlocks = getMatchPatternList(regexMonospaced, html);
+        checkNestedMarkers(regexItalic, regexMonospaced, boldBlocks);
+        checkNestedMarkers(regexBold, regexItalic, monospacedBlocks);
+        checkNestedMarkers(regexBold, regexMonospaced, italicBlocks);
 
         html = html.replaceAll(regexBold, "<b>$1</b>");
         checkCopy = checkCopy.replaceAll(regexBold, "boldBlock");
@@ -52,6 +56,7 @@ public class Converter {
             html = html.replace("PREBLOCK" + i, "<pre>" + preBlocks.get(i) + "</pre>");
         }
         html = html.replaceAll("<p><pre>(.+?)</pre></p>", "<pre>$1</pre>");
+        //System.out.println(checkCopy);
         checkForUnbalancedMarkers(checkCopy);
         return html;
     }
@@ -73,12 +78,38 @@ public class Converter {
     private int countOccurrences(String text, String substring) {
         int count = 0;
         int idx = 0;
-
         while ((idx = text.indexOf(substring, idx)) != -1) {
+            boolean beforeIsMatch = idx > 0 && Character.toString(text.charAt(idx - 1)).matches("[A-Za-z0-9,\\u0400-\\u04FF]");
+            boolean afterIsMatch = idx + substring.length() < text.length() && Character.toString(text.charAt(idx + substring.length())).matches("[A-Za-z0-9,\\u0400-\\u04FF]");
+            if (!((!beforeIsMatch && !afterIsMatch) || (beforeIsMatch && afterIsMatch))) {
+                count++;
+            }
             idx += substring.length();
-            count++;
         }
-
         return count;
+    }
+
+    private List<String> getMatchPatternList(String regex, String html) {
+        List<String> regexBlocks = new ArrayList<>();
+        Pattern regexPatten = Pattern.compile(regex, Pattern.DOTALL);
+        Matcher boldMatcher = regexPatten.matcher(html);
+        while (boldMatcher.find()) {
+            regexBlocks.add(boldMatcher.group(1));
+        }
+        return regexBlocks;
+    }
+
+    private void checkNestedMarkers(String regex1, String regex2, List<String> blocks) throws Exception {
+        Pattern regex1Pattern = Pattern.compile(regex1, Pattern.DOTALL);
+        Pattern regex2Pattern = Pattern.compile(regex2, Pattern.DOTALL);
+        for (String block : blocks) {
+            Matcher regex1Matcher = regex1Pattern.matcher(block);
+            Matcher regex2Matcher = regex2Pattern.matcher(block);
+            boolean matcher1 = regex1Matcher.find();
+            boolean matcher2 = regex2Matcher.find();
+            if (matcher1 | matcher2) {
+                throw new Exception("ERROR: There is nested markers");
+            }
+        }
     }
 }
